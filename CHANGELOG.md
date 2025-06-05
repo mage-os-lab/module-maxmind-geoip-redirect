04fbf7c3ba7d610cae0227ca33e36f88230d196e
Samuele Martini <samuele.rw@gmail.com>
2025-06-05 10:12:05 +0200

    Replaced Phar with splitbrain/php-archive for tar.gz extraction in cron job

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [1.1.0] - 2025-06-05
+### Changed
+- Replaced dependency on PHP Phar extension with `splitbrain/php-archive`
+- Updated code to handle ZIP and TAR archives using the new library (pure PHP implementation)
+
 ## [1.0.0] - 2025-05-30
 ### Added
 - Complete README documentation
--- Cron/IpDatabaseImport.php
+++ Cron/IpDatabaseImport.php
@@ -8,7 +8,7 @@ use MageOS\MaxMindGeoipRedirect\Helper\ModuleConfig;
 use Magento\Framework\HTTP\Client\Curl;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem\Io\File;
-use PharDataFactory;
+use splitbrain\PHPArchive\Tar;
 use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Exception\LocalizedException;
 
@@ -19,14 +19,14 @@ class IpDatabaseImport
      * @param Curl $curl
      * @param DirectoryList $directoryList
      * @param File $file
-     * @param PharDataFactory $pharDataFactory
+     * @param Tar $tar
      */
     public function __construct(
         protected ModuleConfig $moduleConfig,
         protected Curl $curl,
         protected DirectoryList $directoryList,
         protected File $file,
-        protected PharDataFactory $pharDataFactory
+        protected Tar $tar
     ) {
     }
 
@@ -87,18 +87,14 @@ class IpDatabaseImport
      */
     protected function extractTarGzFile(string $tarGz, string $maxMindPath): void
     {
-        $tar = str_replace('.gz', '', $tarGz);
         $extractPath = $this->directoryList->getPath(DirectoryList::VAR_DIR) . DIRECTORY_SEPARATOR . AttributeProvider::EXTRACT_PATH;
-        $geoLiteDatabaseFile = '';
 
-        $gzToTar = $this->pharDataFactory->create(['filename' => $tarGz]);
-        $gzToTar->decompress();
-
-        $tarToFolder = $this->pharDataFactory->create(['filename' => $tar]);
-        $tarToFolder->extractTo($extractPath);
+        $this->tar->open($tarGz);
+        $this->tar->extract($extractPath);
 
         $this->file->cd($extractPath);
 
+        $geoLiteDatabaseFile = '';
         foreach ($this->file->ls(File::GREP_DIRS) as $folder) {
             $geoLiteDatabaseFile = $extractPath . DIRECTORY_SEPARATOR;
             $geoLiteDatabaseFile .= $folder['text'] . DIRECTORY_SEPARATOR;
@@ -110,7 +106,6 @@ class IpDatabaseImport
         $this->file->mv($geoLiteDatabaseFile, $maxMindPath . DIRECTORY_SEPARATOR . AttributeProvider::GEOLITE2_COUNTRY_DB);
 
         $this->file->rm($tarGz);
-        $this->file->rm($tar);
         $this->file->rmdir($extractPath, true);
     }
 }
