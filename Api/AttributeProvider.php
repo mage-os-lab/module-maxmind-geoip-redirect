01460b24fe59f9aafb99279c23c9fcd2c2c44d50
Samuele Martini <samuele.rw@gmail.com>
2026-02-12 17:50:05 +0100

    Convert AttributeProvider from interface to final class

--- Api/AttributeProvider.php
+++ Api/AttributeProvider.php
@@ -2,15 +2,17 @@
 
 namespace MageOS\MaxMindGeoipRedirect\Api;
 
-interface AttributeProvider
+final class AttributeProvider
 {
-    const CHECK_IP_API = 'api';
-    const CHECK_IP_LOCAL = 'local';
-    const IP_DB_DIRECTORY = 'maxmind' . DIRECTORY_SEPARATOR . 'geolite2';
-    const EXTRACT_PATH = self::IP_DB_DIRECTORY . DIRECTORY_SEPARATOR . 'extracted';
-    const GEOLITE2_COUNTRY_DB_TAR_GZ = 'GeoLite2-Country.tar.gz';
-    const GEOLITE2_COUNTRY_DB = 'GeoLite2-Country.mmdb';
-    const GEOLITE2_HOST = 'geolite.info';
-    const MAXMIND_COOKIE = 'maxmind_redirect';
-    const EVENT_DISPATCH_PREFIX = 'maxmind_';
+    public const CHECK_IP_API = 'api';
+    public const CHECK_IP_LOCAL = 'local';
+    public const IP_DB_DIRECTORY = 'maxmind' . DIRECTORY_SEPARATOR . 'geolite2';
+    public const EXTRACT_PATH = self::IP_DB_DIRECTORY . DIRECTORY_SEPARATOR . 'extracted';
+    public const GEOLITE2_COUNTRY_DB_TAR_GZ = 'GeoLite2-Country.tar.gz';
+    public const GEOLITE2_COUNTRY_DB = 'GeoLite2-Country.mmdb';
+    public const GEOLITE2_HOST = 'geolite.info';
+    public const MAXMIND_COOKIE = 'maxmind_redirect';
+    public const EVENT_DISPATCH_PREFIX = 'maxmind_';
+
+    private function __construct() {}
 }
